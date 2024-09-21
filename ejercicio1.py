df6d79cd3cfd2151eb230357d1683b0eace1656c
iamtp29 <iamcarlos08@gmail.com>
2024-09-21 07:40:35 -0500

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+# ejercicio 1
+contrasena="Contraseñasecreta45" 
+contrasena1 = input("Por favor, ingresa la contraseña: ")
+if contrasena.lower()==contrasena1.lower():
+  print("Contraseña correcta")
+else:
+  print("Contraseña incorrecta")
