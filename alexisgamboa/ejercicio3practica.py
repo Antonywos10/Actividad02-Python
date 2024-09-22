08b6be910b7061947e58878723dea6c8e814c5ae
Antony#Wos <113403007+Antonywos10@users.noreply.github.com>
2024-09-21 19:16:41 -0500

    Update ejercicio3practica.py

---
+++ alexisgamboa/ejercicio3practica.py
@@ -0,0 +1,15 @@
+#ejercicio3
+renta = float(input("Introduce tu renta anual: "))
+
+if renta < 10000:
+    tipo_impositivo = "10%"
+elif 10000 <= renta < 20000:
+    tipo_impositivo = "15%"
+elif 20000 <= renta < 35000:
+    tipo_impositivo = "20%"
+elif 35000 <= renta < 60000:
+    tipo_impositivo = "25%"
+else:
+    tipo_impositivo = "45%"
+
+print(f"Tu tipo impositivo es: {tipo_impositivo}%")
