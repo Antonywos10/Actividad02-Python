5e7716a8dda36f719ecdb58f8fe64791020c025e
Antony#Wos <113403007+Antonywos10@users.noreply.github.com>
2024-09-21 19:10:04 -0500

    Update ejercicio4.py

--- AntonyYucra/ejercicio4.py
+++ AntonyYucra/ejercicio4.py
@@ -1,9 +1,8 @@
 def calcular_precio_entrada():
     try:
-        # Solicitar la edad del cliente
+        
         edad = int(input("Introduce la edad del cliente: "))
 
-        # Determinar el precio según la edad
         if edad < 4:
             print("La entrada es gratis.")
         elif 4 <= edad <= 18:
@@ -13,5 +12,5 @@ def calcular_precio_entrada():
     except ValueError:
         print("Por favor, introduce un número válido para la edad.")
 
-# Ejecutar la función
+
 calcular_precio_entrada()
